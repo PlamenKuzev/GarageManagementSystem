a8cadd01ad771e2c575b043e6ba055a15b0885fd
Plamen_K <plamenkuzev11@gmail.com>
2026-01-28 06:41:01 +0200

    Initial project setup with ASP.NET Core MVC

---
+++ GarageManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GarageManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
