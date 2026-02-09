4879faf564781bda9e4052051ffa69b910101275
Plamen_K <plamenkuzev11@gmail.com>
2026-02-09 08:21:29 +0200

    Add light and dark mode background gradients for dashboard polish

--- GarageManagementSystem/obj/Debug/net8.0/GarageManagementSystem.AssemblyInfo.cs
+++ GarageManagementSystem/obj/Debug/net8.0/GarageManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarageManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe4c2e74cac5e1054a754101345a6dea2220ebfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728161da7f61379ff4eb60b745cab44b9922d0d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarageManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarageManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
